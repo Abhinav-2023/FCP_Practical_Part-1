a074781385538ebef5c7b30b80aaaca847640755
Abhinav-2023 <abhinavsingh@ece.du.ac.in>
2023-12-23 21:06:37 +0530

    commit

--- Question_1.c
+++ Question_1.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 float CalculateSimpleInterest(float principal, float rate, float time){
@@ -30,25 +29,4 @@ void main()
 float CalculateSimpleInterest(float principal, float rate, float time){
     float SimpleInterest = principal * rate * time / 100;
     return SimpleInterest;
-}
-
-void main()
-{
-    float principal, rate, time;
-    
-    printf("Enter principal amount: ");
-    scanf("%f", &principal);
-
-    printf("Enter interest rate: ");
-    scanf("%f", &rate);
-
-    rate /= 100;
-
-    printf("Enter time in years: ");
-    scanf("%f", &time);
-
-    float SimpleInterest = CalculateSimpleInterest(principal, rate, time);
-
-    printf("Simple interest is : %f\n", SimpleInterest);
->>>>>>> 92f2445936b1fe2708c59d9b7530c1e87a114095
 }
